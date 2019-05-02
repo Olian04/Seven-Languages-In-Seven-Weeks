5b72e00964aab81746c5d43092e1857a4c930cff
Oliver <rift95@live.se>
2019-05-02 15:36:41 +0200

    finnished day 1

--- Ruby/day1.rb
+++ Ruby/day1.rb
@@ -1,1 +1,23 @@
-puts 'Hello World!'
+puts 'Hello, world.'
+
+hello_ruby = 'Hello, Ruby.'
+ruby_index = hello_ruby.index 'Ruby.'
+puts "Index of \"Ruby.\" in \"Hello, Ruby.\" is #{ruby_index}"
+
+10.times { print 'Oliver ' }
+puts ''
+
+10.times { |i| puts "This is sentence number #{i + 1}" }
+
+def guess_number
+  correct_number = rand(10)
+  guess = nil
+  print 'Guess a number between 0 and 10: '
+  until guess == correct_number
+    guess = gets.to_i
+    print 'Incorrect, please guess again: ' unless guess == correct_number
+  end
+  puts 'Congratulations! You guessed correctly!'
+end
+
+guess_number
